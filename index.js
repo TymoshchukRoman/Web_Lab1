21ddd0598381915afc2bfa786031a63880954764
Roman Tymoshchuk <rtymoshchuk@amcbridge.com>
2022-09-26 16:22:32 +0300

    Ukr language added.

---
+++ index.js
@@ -0,0 +1,37 @@
+﻿function onEnClick() {
+    document.getElementById("hello").innerText = "I'm";
+    document.getElementById("name").innerText = "Roman Tymoshchuk";
+    document.getElementById("ageTitle").innerText = "Age:";
+    document.getElementById("ageText").innerText = "19";
+    document.getElementById("locationTitle").innerText = "Location:";
+    document.getElementById("locationText").innerText = "Dubno, Ukraine";
+    document.getElementById("groupTitle").innerText = "Group:";
+    document.getElementById("groupText").innerText = "KP-03";
+    document.getElementById("hobbiesTitle").innerText = "Hobbies:";
+    document.getElementById("hobbiesText").innerText = "Chess, football, computer games";
+    document.getElementById("expectationsTitle").innerText = "My expectations:";
+    document.getElementById("expectationsText").innerText = "I chose this course to learn another branch of programming. I expect to get thorough knowledge of web development.";
+    document.getElementById("techTitle").innerText = "Favorite technologies:";
+    document.getElementById("techText").innerText = "Python, C# .NET, OpenGL";
+    document.getElementById("expTitle").innerText = "Work experience:";
+    document.getElementById("exphText").innerText = "9 months of engineering software development";
+}
+
+function onUkrClick() {
+    document.getElementById("hello").innerText = "Я";
+    document.getElementById("name").innerText = "Роман Тимощук";
+    document.getElementById("ageTitle").innerText = "Вік:";
+    document.getElementById("ageText").innerText = "19";
+    document.getElementById("locationTitle").innerText = "Місце проживання:";
+    document.getElementById("locationText").innerText = "Дубно, Україна";
+    document.getElementById("groupTitle").innerText = "Група:";
+    document.getElementById("groupText").innerText = "КП-03";
+    document.getElementById("hobbiesTitle").innerText = "Хобі:";
+    document.getElementById("hobbiesText").innerText = "Шахи, футбол, комп'ютерні ігри";
+    document.getElementById("expectationsTitle").innerText = "Мої очікування:";
+    document.getElementById("expectationsText").innerText = "Я обрав цей курс, щоб опанувати ще одну галузь в програмуванні. Очікую отримати грунтовні знання у сфері веброзробки.";
+    document.getElementById("techTitle").innerText = "Улюблені технології:";
+    document.getElementById("techText").innerText = "Python, C# .NET, OpenGL";
+    document.getElementById("expTitle").innerText = "Досвід роботи:";
+    document.getElementById("exphText").innerText = "9 місяців розробки інженерного програмного забезпечення";
+}
